cbc466d72d697837428cfeb83462c3801fc37d7d
Praneel S P <praneel18@hotmail.com>
2025-06-10 15:10:42 -0400

    10th june

--- script.js
+++ script.js
@@ -9,6 +9,20 @@ document.addEventListener("DOMContentLoaded", () => {
   const menuToggle = document.querySelector(".menu-toggle");
   const navLinks = document.querySelector(".navbar nav ul");
 
+  const navItems = document.querySelectorAll(".navbar nav ul li a");
+
+  navItems.forEach((item) => 
+  {
+    item.addEventListener("click", () => 
+      {
+      if (navLinks.classList.contains("active")) 
+        {
+        navLinks.classList.remove("active");
+      }
+    });
+  });
+
+
   if (menuToggle && navLinks) {
     menuToggle.addEventListener("click", () => {
       navLinks.classList.toggle("active");
